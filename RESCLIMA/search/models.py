fdbe3a6d6347f666c5274948533f6fa6b38e6d4c
Vivi Laurido <vlaurido@espol.edu.ec>
2018-07-24 09:52:30 -0500

    Se anadio modelo para categorias

--- RESCLIMA/search/models.py
+++ RESCLIMA/search/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from Layer.models import Layer
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    variables = models.ManyToManyField(Layer, blank = True)
+
+    def __unicode__(self):
+        return "%s-%s" % (self.name)
+    def __str__(self):
+        return "%s-%s" % (self.name)
+
+    class Meta:
+        verbose_name = "Categoria"
+        verbose_name_plural = "Categorias"
