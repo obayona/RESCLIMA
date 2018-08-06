192d963edf373b2d8023c28c5b870161614320d4
gaalocastillo <gadacast@espol.edu.ec>
2018-08-06 08:56:12 -0500

    Reading Sky2 station id from external file to store records on database.

--- StationScripts/Sky2/sky2.py
+++ StationScripts/Sky2/sky2.py
@@ -20,19 +20,21 @@ def bloomsky_client(apiUrl, frequency, filename, myConnection):
 	cursor = myConnection.cursor()
 	while(True):
 		tokens = getTokens(filename)
-		for token in tokens:
+		for line in tokens:
+			line = line.strip()
+			fields = line.split(',')
+			idStation = fields[0]
+			token = fields[1] 
 			request = urllib2.Request(API_URL)
 			request.add_header('Authorization', token)
 			response = urllib2.urlopen(request).read()
 			json1_data = json.loads(response)[0]
 			readings = json1_data['Data']
 			timestamp = readings['TS']
-			idStation = 1
 			dt = datetime.datetime.fromtimestamp(timestamp)
 			cursor.execute('Insert into "TimeSeries_measurement" ("datetime", "idStation_id", "readings") values (%s, %s, %s)', (dt, idStation, str(readings)))
 			myConnection.commit()
 		time.sleep(frequency)
-
 HOSTNAME = 'localhost'
 USERNAME = 'obayona'
 PASSWORD = 'EloyEcuador93'
