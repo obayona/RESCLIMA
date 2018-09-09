46dc6b36ca08d07e38cb6599b7ed5df023f84bae
obayona <obayona@espol.edu.ec>
2018-09-09 05:15:47 -0500

    Home, con lista de capas con checkbox

--- RESCLIMA/main/static/main/js/layers_component.js
+++ RESCLIMA/main/static/main/js/layers_component.js
@@ -2,8 +2,21 @@ Vue.component("layers_component",{
 	template: `
 		<div>
 			<div v-if="shared.results.length > 0">
+				<div>
+					<button>Visualizar</button>
+					<button>Descargar</button>
+				</div>
 				<div v-for="layer in shared.results">
-					{{layer.title}}
+					<div class="card">
+						<div class="card-content">
+							<span class="card-title">
+								<input type="checkbox" v-bind:id="layer.id" v-model="layer.selected">
+								<label v-bind:for="layer.id"></label>
+								{{layer.title}}
+							</span>
+							<p>{{layer.abstract}}</p>
+						</div>
+					</div>		
 				</div>
 			</div>
 			<div v-else>No hay resultados</div>
