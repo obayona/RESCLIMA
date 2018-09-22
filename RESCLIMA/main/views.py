15a2477c2cb3c61f5bc15b376277c3c411da3926
obayona <obayona@espol.edu.ec>
2018-09-22 14:56:25 -0500

    Mejoras formulario subida de shapefile

--- RESCLIMA/main/views.py
+++ RESCLIMA/main/views.py
@@ -7,30 +7,30 @@ from django.contrib.auth.decorators import login_required
 
 #METODOS DE ACCESO
 def login(request):
-    if (request.method == 'POST'):
-        username = request.POST['username']
-        password = request.POST['password']
-        user = authenticate(username=username, password=password)
-        if user is not None:
-            auth_login(request, user)
-            return redirect('profile')
-        else:
-            print("ERROR DE AUTENTICACION...")
-            return render(request,'main/login.html', {'error':True})
-    else:
-        return render(request, 'main/login.html', {})
+	if (request.method == 'POST'):
+		username = request.POST['username']
+		password = request.POST['password']
+		user = authenticate(username=username, password=password)
+		if user is not None:
+			auth_login(request, user)
+			return redirect('profile')
+		else:
+			print("ERROR DE AUTENTICACION...")
+			return render(request,'main/login.html', {'error':True})
+	else:
+		return render(request, 'main/login.html', {})
 
 def logout(request):
-    auth_logout(request)
-    return render(request, 'main/home.html', {})
+	auth_logout(request)
+	return render(request, 'main/home.html', {})
 
 def noAccess(request):
-    return render(request, 'main/noAccess.html', {})
+	return render(request, 'main/noAccess.html', {})
 
 def home(request):
-    return render(request,"main/home.html")
+    	return render(request,"main/home.html")
 
 @login_required(login_url='noAccess')
 def profile(request):
-    researcher = request.user.researcher
-    return render(request, 'main/profile.html', {'researcher': researcher, })
+	researcher = request.user.researcher
+	return render(request, 'main/profile.html', {'researcher': researcher, })
