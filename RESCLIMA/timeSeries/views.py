e832e40b985f6924098b9044fb2065da44cd80ae
obayona <obayona@espol.edu.ec>
2018-10-02 11:34:31 -0500

    mejora en los formularios del app timeSeries

--- RESCLIMA/timeSeries/views.py
+++ RESCLIMA/timeSeries/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.http import HttpResponse
-from timeSeries.forms import *
 from parsers.csv_parsers_module import *
 from django.contrib import messages
 from models import StationType, Station
@@ -10,82 +9,83 @@ from django.contrib.auth.decorators import login_required
 
 @login_required(login_url='noAccess')
 def show_options(request):
-    return render(request,"home_series.html")
+	return render(request,"home_series.html")
 
 
 def addSensor(data):
-    try:
-        stationType_id = int(data["stationType"]);
-        serialNum = data["serialNum"];
-        latitude = float(data["latitude"]);
-        longitude = float(data["longitude"]);
-        frequency = data["frequency"];
-        token = data["token"];
+	try:
+		stationType_id = int(data["stationType"]);
+		serialNum = data["serialNum"];
+		latitude = float(data["latitude"]);
+		longitude = float(data["longitude"]);
+		frequency = data["frequency"];
+		token = data["token"];
 
-        stationType = StationType.objects.get(id=stationType_id);
-        automatic = stationType.automatic;
-        station = Station();
-        station.serialNum = serialNum;
-        station.location = Point(longitude,latitude);
-        station.active = True;
-        station.stationType = stationType;
+		stationType = StationType.objects.get(id=stationType_id);
+		automatic = stationType.automatic;
+		station = Station();
+		station.serialNum = serialNum;
+		station.location = Point(longitude,latitude);
+		station.active = True;
+		station.stationType = stationType;
 
-        if(automatic==True):
-            if(frequency == "" or token == ""):
-                return "Error: faltan argumentos";
-            frequency = float(frequency);
-            if(frequency<=0):
-                return "Error: frecuencia debe ser mayor que cero"
-            station.frequency = frequency;
-            station.token = token;
+		if(automatic==True):
+			if(frequency == "" or token == ""):
+				return "Error: faltan argumentos";
+			frequency = float(frequency);
+			if(frequency<=0):
+				return "Error: frecuencia debe ser mayor que cero"
+			station.frequency = frequency;
+			station.token = token;
 
-        station.save();
-    except Exception as e:
-        return "Error " + str(e)
+		station.save();
+	except Exception as e:
+		return "Error " + str(e)
 
-    return None
+	return None
 
 def new_sensor(request):
-    if request.method == "GET":
-        station_types = StationType.objects.all()
-        options = {'station_types':station_types}
-        return render(request, 'new_sensor.html',options)
-    elif request.method == "POST":
-        err_msg = None;
-        form = StationForm(request.POST)
-        if form.is_valid():
-            err_msg = addSensor(request.POST);
-        else:
-            err_msg = form.errors
+	if request.method == "GET":
+		station_types = StationType.objects.all()
+		options = {'stationTypes':station_types}
+		return render(request, 'new_sensor.html',options)
+	elif request.method == "POST":
+		err_msg = None;
+		'''
+		form = StationForm(request.POST)
+		if form.is_valid():
+			err_msg = addSensor(request.POST);
+		else:
+			err_msg = form.errors
 
-        if(err_msg==None):
-            return HttpResponse("OK")
-        else:
-            return HttpResponse(err_msg);
+		if(err_msg==None):
+			return HttpResponse("OK")
+		else:
+			return HttpResponse(err_msg);
+		'''
+		return HttpResponse("OK")
 
 
 def upload_file(request):
-    if request.method == "GET":
-        form = UploadFileForm()
-        return render(request, 'base_form.html', {'form': form})
-    elif request.method == "POST":
-        err_msg = None;
-        form = UploadFileForm(request.POST, request.FILES)
-        if form.is_valid():
-            stationType_id = int(form.cleaned_data['stationType']);
-            stationType = StationType.objects.get(id=stationType_id);
-            stationType_str = str(stationType);
-            file = request.FILES['file']
-            if stationType_str == "HOBO-MX2300":
-                err_msg = parseHOBO(file);
-            elif stationType_str == "":
-                err_msg = parseDataLogger(file);
-            else:
-                err_msg = "Error: no se reconoce ese tipo de estacion";
-        else:
-            err_msg = form.errors
+	if request.method == "GET":
+		station_types = StationType.objects.filter(automatic=False)
+		params = {"stationTypes":station_types}
+		return render(request, 'import_file.html', params)
+	elif request.method == "POST":
+		err_msg = None;
+		stationType_id = request.POST['stationType'];
+		stationType = StationType.objects.get(id=stationType_id);
+		stationType_str = str(stationType);
+		file_ptr = request.FILES['file']
+		if stationType_str == "HOBO-MX2300":
+			err_msg = parseHOBO(file_ptr);
+		elif stationType_str == "":
+			err_msg = parseDataLogger(file_ptr);
+		else:
+			err_msg = "Error: no se reconoce ese tipo de estacion";
 
-        if(err_msg=="Success"):
-            return HttpResponse("OK")
-        else:
-            return HttpResponse(err_msg);
+		if(err_msg=="Success"):
+			return HttpResponse("OK")
+		else:
+			return HttpResponse(err_msg);
+
