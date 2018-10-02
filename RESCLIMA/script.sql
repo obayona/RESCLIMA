7bb44afd0dff44a287a65c5a665ad441b38fc433
obayona <obayona@espol.edu.ec>
2018-10-02 14:36:15 -0500

    Organizacion del proyecto

--- RESCLIMA/script.sql
+++ RESCLIMA/script.sql
@@ -1,28 +1,19 @@
 
-UPDATE "layer_layer" SET textsearchable_index = to_tsvector('spanish',coalesce(title,'') || ' ' || coalesce(abstract,''));
-
-CREATE INDEX textsearch_idx ON "layer_layer" USING GIN (textsearchable_index);
-
-SELECT title,id FROM "Layer_layer" WHERE textsearchable_index @@ plainto_tsquery('spanish','Playas');
-
-SELECT title, id, abstract, ts_rank_cd(textsearchable_index, query) AS rank
-FROM "Layer_layer", plainto_tsquery('spanish','Playas') query
-WHERE query @@ textsearchable_index
-ORDER BY rank DESC
-LIMIT 10;
+/*Index for ts_index field*/
+CREATE INDEX textsearch_idx ON "layer_layer" USING GIN (ts_index);
 
 /*Trigger that fills the ts_index column before any insert or update in the Layer table*/
 CREATE TRIGGER tsvector_update_layer BEFORE INSERT OR UPDATE
 ON "layer_layer" FOR EACH ROW EXECUTE PROCEDURE
-tsvector_update_trigger("ts_index", 'pg_catalog.spanish', title, abstract);
-
+tsvector_update_trigger("ts_index", 'pg_catalog.spanish', title, abstract, categories_string);
 
 /*Trigger that fills the ts_index column before any insert or update in the Variable table*/
 CREATE TRIGGER tsvector_update_variable BEFORE INSERT OR UPDATE
 ON "timeSeries_variable" FOR EACH ROW EXECUTE PROCEDURE
-tsvector_update_trigger("ts_index", 'pg_catalog.spanish', name);
+tsvector_update_trigger("ts_index", 'pg_catalog.spanish', name, categories_string);
 
-
+/*Function to insert a measurement that comes from a
+meteorologycal station of type Sky2*/
 CREATE OR REPLACE FUNCTION InsertSky2Measurements
 (idStation INTEGER, ts_in TIMESTAMP,readings JSON)
 RETURNS void AS $$
@@ -36,3 +27,13 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 
+
+/*Example of a query of fts over the variable table*/
+/*
+SELECT id,name FROM "timeSeries_variable" WHERE ts_index @@ plainto_tsquery('spanish','velocida');
+
+SELECT title, id, abstract, ts_rank_cd(textsearchable_index, query) AS rank
+FROM "Layer_layer", plainto_tsquery('spanish','Playas') query
+WHERE query @@ textsearchable_index
+ORDER BY rank DESC
+LIMIT 10;*/
