3d6f097749c6729f5f3479a2527fbd1a09ca9a72
obayona <obayona@espol.edu.ec>
2018-10-11 15:51:08 -0500

    opcion zoom a la capa

--- RESCLIMA/layer/static/layer/js/layers_component.js
+++ RESCLIMA/layer/static/layer/js/layers_component.js
@@ -1,35 +1,67 @@
+
 Vue.component("layers_component",{
 	template: `
-		<!--Contenedor corredizo, se abre cuando open es true-->
+		<!--Contenedor corredizo, se abre cuando open es True-->
 		<div id="layerContainer"
 		v-bind:style="[open?{'left':'0px'}:{'left':'-400px'}]">
+			
+			<!-- Boton que abre y cierra el contenedor-->
 			<div id="layerContainerOpen"
 			class="gradient-45deg-light-blue-cyan"
 			 v-on:click.prevent="open=!open">
+				<!-- Dependiendo del valor de open se muestra un icono diferente-->
 				<i v-if="open" class="material-icons">keyboard_arrow_down</i>
 				<i v-else class="material-icons">keyboard_arrow_up</i>
 				Capas
+				<!-- Dependiendo del valor de open se muestra un icono diferente-->
 				<i v-if="open" class="material-icons">keyboard_arrow_down</i>
 				<i v-else class="material-icons">keyboard_arrow_up</i>
 			</div>
+
+			<!-- Contenedor con la lista de capas -->
 			<div id="layerList" >
+				<!-- Si hay capas en shared.layers se muestra la lista-->
 				<div v-if="shared.layers.length>0">
+					<!-- Contenedor de la capa -->
+					<!-- se intera sobre el array shared.layers -->
+					<!-- Si la capa es igual a currentLayer, se pinta-->
 					<div v-for="layer in shared.layers"
 					class="layerItem"
 					v-bind:style="[shared.currentLayer.id==layer.id?{'background':'#EFEBE9'}:{}]">
+						<!-- Si la capa tiene estado uninitialized-->
+						<!-- Se muestran animaciones-->
 						<div v-if="layer.state=='uninitialized'">
 							<div style="width:100px;height:20px;margin:10px;" class="animated-background"></div>
 							<div style="width:200px;height:20px;margin:10px;" class="animated-background"></div>
 							<div style="width:20px;height:20px;margin:10px;" class="animated-background"></div>
 						</div>
+						<!-- Si la capa tiene estado distinto a uninitialized-->
+						<!-- Se muestran los metadatos de la capa -->
 						<div v-if="layer.state!='uninitialized'">
+							<!-- Opcion zoom a la capa-->
+							<div style="height:100%;position: relative;">
+								<span title="Zoom a la capa"
+								class="btnZoom" v-on:click.prevent="zoomToLayer(layer)">
+									<i class="material-icons left">zoom_in</i>
+								</span>
+							</div>
+
+							<!-- El titulo de la capa-->
+							<!-- Si se da click es esta capa, se convierte en-->
+							<!-- currentLayer -->
 							<h5 class="header layerTitle"
 							 v-on:click.prevent="shared.currentLayer=layer">
 								{{layer.title}}
 							</h5>
+							<!-- El resumen de la capa -->
 							<p>{{layer.abstract}}</p>
+							<!-- La fecha de la capa-->
 							<h6>{{layer.data_date}}</h6>
+
+							<!-- Contenedor con opciones de la capa -->
+							<!-- Solo se muestra si el estado de la capa es loaded-->
 							<div class="layerOptions" v-if="layer.state=='loaded'">
+								<!-- Slider para controlar la transparencia-->
 								<p class="range-field">
 									<label>Transparencia</label>
 									<input type="range" 
@@ -39,21 +71,27 @@ Vue.component("layers_component",{
 									v-model="layer.opacity"
 									v-on:input="changeOpacity(layer)"/>
 								</p>
+								<!-- Links para descargar y remover la capa-->
 								<div>
 									<a style="padding:10px;color:green" href="#"><i class="material-icons">file_download</i>Descargar</a>
 									<a style="padding:10px;color:red" href="#"><i class="material-icons">delete</i>Eliminar</a>
 								</div>
 							</div>
+							<!-- Si la capa no tiene estado loaded-->
+							<!-- no se muestran las opciones de la capa,-->
+							<!-- se muestra una animacion-->
 							<div v-else>
 								<div class="animated-background" style="height:80px">Cargando...</div>
 							</div>
 						</div>
 					</div>
 				</div>
+				<!-- Si no hay capas en shared.layers se muestra un mensaje-->
 				<div v-else>
 					<p>No hay capas</p>
 				</div>
 			</div>
+
 		</div>
 	`,
 	mounted(){
@@ -150,6 +188,16 @@ Vue.component("layers_component",{
 			var opacity = layer["opacity"];
 			var openlayer_layer = layer["openlayer_layer"];
 			openlayer_layer.setOpacity(1-opacity/100);
+		},
+		/*
+		Metodo para realizar un zoom a la capa
+		*/
+		zoomToLayer(layer){
+			var extent = layer["extent"];
+			if(extent){
+				var map = this.shared.map;
+				map.zoomToExtent(extent);
+			}
 		}
 	}
 })
