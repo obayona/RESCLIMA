8eb0f2e21e9d22b4f83ff5759120de3622c43dfa
obayona <obayona@espol.edu.ec>
2018-10-29 16:00:55 -0500

    Se agrego el boton, para descargar datos de series de tiempo

--- RESCLIMA/timeSeries/static/timeSeries/js/serie_component.js
+++ RESCLIMA/timeSeries/static/timeSeries/js/serie_component.js
@@ -1,96 +1,138 @@
 
 Vue.component("serie_component",{
 	template: `
-		<div class="card">
-			<!-- si el estado es uninitialized -->
-			<!-- se muestra una animacion -->
-			<div 
-			 v-bind:style="[variable.state=='uninitialized'?{'display':''}:{'display':'none'}]">
-				<div class="card-header cyan accent-4">
-					<div class="card-title white-text">
-						 <h4 class="card-title">Cargando...</h4>
+		<!--Contenedor del component -->
+		<div>
+			<!-- Si el estado de la variable es failed-->
+			<!-- se muestra un mensaje de error-->
+			<div class="card"
+			 v-bind:style="[variable.state=='failed'?{'display':''}:{'display':'none'}]" >
+				<div class="row">
+					<div style="text-align:center;margin:100px;">
+						<h5><i class="material-icons" style="color:red;">error</i>
+						Error en la variable</h5>
 					</div>
-				</div>
-				<div class="card-content">
-					<div style="width:100%;height:500px;margin:10px;" class="animated-background"></div>
 				</div>
 			</div>
-					
-			<!-- Si la serie tiene estado distinto a uninitialized-->
-			<!-- Se muestran los metadatos de la serie -->
-			<div v-bind:style="[variable.state!='uninitialized'?{'display':''}:{'display':'none'}]">
-				<div class="card-header cyan accent-4">
-					<div class="card-title white-text">
-						 <h4 class="card-title">{{variable.name}} vs tiempo</h4>
+			<!-- Si el estado es diferente de failed-->
+			<!-- se muestra la variable-->
+			<div class="card"
+			 v-bind:style="[variable.state!='failed'?{'display':''}:{'display':'none'}]">
+				<!-- si el estado es uninitialized -->
+				<!-- se muestra una animacion -->
+				<div v-bind:style="[variable.state=='uninitialized'?{'display':''}:{'display':'none'}]">
+					<div class="card-header cyan accent-4">
+						<div class="card-title white-text">
+							 <h4 class="card-title">Cargando...</h4>
+						</div>
+					</div>
+					<div class="card-content">
+						<div style="width:100%;height:500px;margin:10px;" class="animated-background"></div>
 					</div>
 				</div>
-				<div class="card-content">
-					<!-- Contenedor para el plot-->
-					<div class="row">
-						<div class="col s10">
-							<div v-bind:data-plot-id="variable.id"></div>
-						</div>
-						<div class="col s2">
-							<div>
-								<div v-for="station in variable.stations">
-									<!-- Si la estacion tiene estado loading -->
-									<div v-if="station.state == 'loading'">
-										<div style="width:100px;height:20px;margin:10px;" class="animated-background"></div>
-									</div>
-									<!-- Si la estacion tiene estado failed -->
-									<div v-if="station.state == 'failed'">
-										<i class="material-icons" style="color:red;">error</i>
-										<span style="opacity:0.5;color:red;">{{"Estación " + station.id}}</span>
-									</div>
-									<!--Si la traza de la estacion esta cargada -->
-									<!-- se muestra el legend item -->
-									<div class="legendItem"
-									 v-if="station.state == 'loaded'"
-									 v-bind:style="[station.visible?{'opacity':'1'}:{'opacity':'0.5'}]"
-									 v-on:click.prevent="showHideTrace(station)">
-										<strong class="legendSymbol"
-											v-bind:style="{'background':station.color}">
-										</strong>
-										<span>{{"Estación " + station.id}}</span>
+						
+				<!-- Si la variable tiene estado loaded-->
+				<!-- Se muestran los metadatos de la variable -->
+				<div v-bind:style="[variable.state=='loaded'?{'display':''}:{'display':'none'}]">
+					<nav class="nav-wrapper cyan accent-4">
+						<ul class="left"><li><h1 class="logo-wrapper">
+							<a href="#!">
+								<span class="logo-text">{{variable.name}} vs tiempo</span>
+							</a>
+						</h1></li></ul>
+						<ul class="right" v-bind:click.prevent="downloadFile()"><li>
+							<a href="#!" title="Descargar">
+								<i class="material-icons">file_download</i>
+							</a>
+						</li></ul>
+					</nav>
+					<div class="card-content">
+						<!-- Solo se muestra si el tipo de dato de la-->
+						<!-- variable es float-->
+						<div v-bind:style="[variable.datatype=='float'?{'display':''}:{'display':'none'}]">
+							<!-- Contenedor para el plot y la leyenda-->
+							<div class="row">
+								<div class="col s10">
+									<!-- Contenedor para el plot -->
+									<div v-bind:data-plot-id="variable.id"></div>
+								</div>
+								<div class="col s2">
+									<!-- Contenedor para la leyenda -->
+									<div>
+										<div v-for="station in variable.stations">
+											<!-- Si la estacion tiene estado loading -->
+											<!-- se muestra una animacion -->
+											<div v-if="station.state == 'loading'">
+												<div style="width:100px;height:20px;margin:10px;" class="animated-background"></div>
+											</div>
+											<!-- Si la estacion tiene estado failed -->
+											<!-- Se muestra un mensaje de error-->
+											<div v-if="station.state == 'failed'">
+												<i class="material-icons" style="color:red;">error</i>
+												<span style="opacity:0.5;color:red;">{{"Estación " + station.id}}</span>
+											</div>
+											<!--Si la traza de la estacion esta cargada -->
+											<!-- se muestra el legend item -->
+											<div class="legendItem"
+											 v-if="station.state == 'loaded'"
+											 v-bind:style="[station.visible?{'opacity':'1'}:{'opacity':'0.5'}]"
+											 v-on:click.prevent="showHideTrace(station)">
+												<strong class="legendSymbol"
+													v-bind:style="{'background':station.color}">
+												</strong>
+												<span>{{"Estación " + station.id}}</span>
+											</div>
+										</div>
 									</div>
 								</div>
+							</div>						
+							<!-- botones de paginacion -->
+							<!-- Si hay mas datos que mostrar, se muestran los botones-->
+							<div class="row"
+								v-if="offset != 0 || offset < max_offset">
+								<ul class="pagination">
+									<li v-bind:class="{disabled: offset == 0}"
+									    v-on:click.prevent="prev()">
+										<a href="#!">
+											<i class="material-icons">chevron_left</i>
+										</a>
+									</li>
+									<li v-bind:class="{disabled: offset >= max_offset}"
+									    v-on:click.prevent="next()">
+										<a href="#!">
+											<i class="material-icons">chevron_right</i>
+										</a>
+									</li>
+								</ul>
+							</div>
+						</div>
+						<!-- Si el tipo de dato de la variable no es float-->
+						<!-- se muestra un mensaje de warning-->
+						<div class="row"
+						 v-bind:style="[variable.datatype!='float'?{'display':''}:{'display':'none'}]">
+							<div style="text-align:center;margin:100px;">
+								<h5><i class="material-icons" style="background:black;color:yellow;">warning</i>
+								No se puede visualizar el tipo de datos {{variable.datatype}}</h5>
 							</div>
 						</div>
 					</div>
-					<!-- botones de paginacion -->
-					<div class="row"
-						v-if="offset != 0 || offset < max_offset">
-						<ul class="pagination">
-							<li v-bind:class="{disabled: offset == 0}"
-							    v-on:click.prevent="prev()">
-								<a href="#!">
-									<i class="material-icons">chevron_left</i>
-								</a>
-							</li>
-							<li v-bind:class="{disabled: offset >= max_offset}"
-							    v-on:click.prevent="next()">
-								<a href="#!">
-									<i class="material-icons">chevron_right</i>
-								</a>
-							</li>
-						</ul>
-					</div>
-				</div>
-
+				</div>	
 			</div>
-			
 		</div>
 	`,
 	props:["variable"],
 	mounted(){
 		var variable = this.variable;
 		var id_variable = variable.id;
+		// se obtiene el contenedor del plot
 		var nodes = document.querySelectorAll('[data-plot-id="'+id_variable+'"]');
 		if(nodes.length==0){
-			console.log("Error fatal");
+			console.log("lo hago failed en mounted",variable)
+			variable.state = "failed";
 			return;
 		}
 		this.container = nodes[0];
+		// se obtiene la informacion de las variables
 		this.getVariableInfo(variable);
 	},
 
@@ -105,6 +147,9 @@ Vue.component("serie_component",{
 	},
 
 	methods:{
+		/*
+		Realiza una peticion ajax para 
+		obetenr los metadados de una variable*/
 		getVariableInfo(variable){
 			var url = "/series/variable/info/"+variable.id;
 			var request = $.get(url);
@@ -114,16 +159,25 @@ Vue.component("serie_component",{
 				variable["unit"]=data["unit"];
 				variable["symbol"]=data["symbol"];
 				variable["datatype"]= data["datatype"];
-				variable["state"]="metadata_loaded";
+				variable["state"]="loaded";
 				// se inicializa el plot
 				self.initializePlot(variable);
-				// se piden los datos
-				self.getMeasurements(variable);
+				// se piden los datos solo si el tipo de 
+				// dato es float
+				if(variable["datatype"]=="float"){
+					self.getMeasurements(variable);	
+				}
+				
 			});
 			request.fail(function(data){
-				console.log("Error",data)
+				console.log("lo hago failed",variable);
+				variable.state = 'failed';
 			});
 		},
+		/*
+		Por cada estacion de la variable,
+		pide los datos de la estacion
+		*/
 		getMeasurements(variable){
 			var stations = variable["stations"];
 			for(var i=0; i<stations.length; i++){
@@ -131,8 +185,12 @@ Vue.component("serie_component",{
 				this.getStationMeasurements(variable,station);
 			}
 		},
+		/*
+		Pide la serie de tiempo de una estacion.
+		*/
 		getStationMeasurements(variable,station){
 			var self = this;
+			// se crea el url			
 			var url = "/series/measurements/?";
 			url += "variable_id="+variable["id"];
 			url += "&station_id="+station["id"];
@@ -144,7 +202,7 @@ Vue.component("serie_component",{
 			}
 			url+="&limit="+this.limit;
 			url+="&offset="+this.offset;
-			console.log(url);
+
 			var request = $.get(url);
 			station["state"]="loading";
 			request.done(function(response){
@@ -161,6 +219,9 @@ Vue.component("serie_component",{
 				station["state"]="failed";
 			});
 		},
+		/*
+		Asigna la serie de tiempo a la estacion
+		*/
 		assingMeasurements(station,measurements){
 			station["x_values"] = [];
 			station["y_values"] = [];
@@ -171,9 +232,10 @@ Vue.component("serie_component",{
 			}
 			station["state"]="loaded";
 		},
+		/*
+		Inicializa el plot usando la libreria Plotly
+		*/
 		initializePlot(variable){
-			// se inicializa el plot
-
 			var data = []
 			var y_title = variable["unit"]+ " ( " + variable["symbol"] + " )";
 			var layout = {
@@ -189,8 +251,12 @@ Vue.component("serie_component",{
 				yaxis:{title: y_title}
 			};
 			Plotly.newPlot(this.container,data,layout,{responsive: true});
+			// se actualiza el plot
 			Plotly.Plots.resize(this.container);
 		},
+		/*
+		Agrega una traza al plot
+		*/
 		addTrace(variable,station){
 			var container = this.container;
 			var data = container.data;
@@ -203,23 +269,29 @@ Vue.component("serie_component",{
 				y:station["y_values"],
 				type: 'scatter',
 				name: 'Estacion '+station["id"],
-				line:{color:station["color"]}
+				line:{color:station["color"]},
+				visible:station["visible"]
 			};
 			Plotly.addTraces(container,[trace]);
 		},
+		/*
+		Muestra u oculta una traza del plot,
+		dependiendo del valor de verdad del
+		atributo "visible" de la estacion
+		*/
 		showHideTrace(station){
-			console.log(station["visible"])
 			station["visible"]=!station["visible"];
-			console.log(station["visible"])
 			var container = this.container;
 			var data = container.data;
 			var trace_index = station["trace_index"];
 			var trace = data[trace_index];
-			console.log(trace)
-			console.log(station)
 			trace.visible = station["visible"];
 			Plotly.redraw(container);
 		},
+		/*
+		Handler del boton prev, en la
+		paginacion. Trae los anteriores datos
+		*/
 		prev(){
 			if(this.offset <= 0){
 				return;
@@ -230,6 +302,10 @@ Vue.component("serie_component",{
 			this.initializePlot(variable);
 			this.getMeasurements(variable);
 		},
+		/*
+		Handler del boton next, en la
+		paginacion. Trae los siguiente datos
+		*/
 		next(){
 			if(this.offset>=this.max_offset){
 				return;
@@ -239,6 +315,13 @@ Vue.component("serie_component",{
 			var variable = this.variable;
 			this.initializePlot(variable);
 			this.getMeasurements(variable);
+		},
+		/*
+		Metodo que solicita al servidor
+		que descargue los datos de una variable como 
+		un archivo*/
+		downloadFile(){
+			console.log("Descargar variable",this.variable)
 		}
 	},
 })
