014d299e9f3daf3fe92e181155e0089ee6e20b03
soccerdroid <mguarand@espol.edu.ec>
2019-04-01 00:12:21 -0500

    fixed bug of required token field form that did not allow to create a station. Adding back-end support for generic stations

--- RESCLIMA/timeSeries/tasks.py
+++ RESCLIMA/timeSeries/tasks.py
@@ -302,4 +302,232 @@ def parseHOBOFile(hoboParams):
 	return result
 
 
+@shared_task
+def parseGenericFile(genericParams):
+	# se obtiene el nombre del archivo
+	fileName = genericParams["fileName"]
+	'''
+	Diccionario con el resultado de la operacion.
+	Este  diccionario  estara dentro de un objeto 
+	celery.result.AsyncResult
+	result = {
+				"error": string  con  mensaje de error, 
+						 si no hay error, esta clave no 
+						 existira o sera None
+				"percent": porcentaje de progreso de la
+						   operacion
+			}
+	'''
+	result = {}
+	# abre el archivo
+	f = None
+	if os.path.isfile(fileName):
+		encoding = getFileEncoding(fileName)
+		# si no se pudo determinar el encoding
+		# del archivo, se prueba con utf-8
+		if(encoding==None):
+			encoding = 'utf-8'
+		f = open(fileName,'r',encoding=encoding)
+	else:
+		result["error"]="Error, se perdio el archivo"+fileName;
+		current_task.update_state(state='FAILURE',meta=result)
+		return result
+
+	'''
+	se comprueba  el numero de  lineas del 
+	archivo se requieren al menos 4 lineas
+	1era linea: contiene el numero de serie de la estacion
+	2da linea: vacio
+	3ra linea: contiene el header del csv
+	4ta linea: datos
+	'''
+	num_lines = count_file_lines(f)
+	if (num_lines<4):
+		result["error"]="Error: archivo sin datos";
+		current_task.update_state(state='FAILURE',meta=result)
+		return result
+
+	# formato de la fecha
+	datetime_format = "%Y-%m-%d %H:%M:%S"
+	# time zone de las fechas de los datos (ej.: UTC,GMT+2,GMT-4,etc)
+	local_tz_str = None;
+	# numero de serie de la estacion
+	serialNum = None;
+	# objeto con datos de la estacion
+	station = None;
+
+	# actualiza el porcentaje de avance de la tarea: 5%
+	result["error"]=None
+	result["percent"]=5
+	current_task.update_state(state='PROGRESS',meta=result)
+
+
+	percent_cont = 0
+
+	
+	# se obtienen las variables de la base de datos
+	temp = Variable.objects.get(name=u"Temperatura")
+	rh = Variable.objects.get(name=u"Humedad relativa")
+	rain = Variable.objects.get(name=u"Lluvia")
+	wind_dir = Variable.objects.get(name=u"Dirección del viento")
+	wind_vel = Variable.objects.get(name=u"Velocidad del viento")
+	wind = Variable.objects.get(name=u"Velocidad de rafagas")
+	lum = Variable.objects.get(name=u"Luminancia")
+	detec_rain = Variable.objects.get(name=u"Dtección de lluvia")
+	press = Variable.objects.get(name=u"Presión")
+	uv = Variable.objects.get(name=u"Indice UV")   #es un numero float
+	variables = [temp,rh,rain,wind_dir,wind_vel,wind,lum,detec_rain,press,uv]
+
+	# se itera el archivo
+	try:
+		for i,line in enumerate(f,1):
+		
+			# si se lee la primera linea del archivo
+			# se recupera el numero serial de la estacion
+			if(i==1):
+				# la primera linea contiene "Serial Number: string"
+				# string es el  numero serial  de la estacion
+				parts = line.split(":")
+				if (len(parts)==2):
+					serialNum = parts[1]
+					# se eliminan caracteres en blanco
+					serialNum = serialNum.strip(' \t\n\r')
+				else:
+					# se borra el archivo
+					os.remove(fileName)
+					result["error"]="Error: no se especifica el numero de serie o ID de la estacion"
+					current_task.update_state(state='FAILURE',meta=result)
+					return result
+
+				# se valida que la estacion existe en la base de datos
+				results=Station.objects.filter(serialNum=serialNum);
+				if(results.count()!=1):
+					# se borra el archivo
+					os.remove(fileName)
+					result["error"]="Error: no se encontro la estacion "+serialNum
+					current_task.update_state(state='FAILURE',meta=result)
+					return result
+				else:
+					station = results[0]
+
+				# se comprueba que el tipo de stacion sea HOBO-MX2300
+				typestation = str(station.stationType)
+				if(typestation!="Otro-Otro"):
+					# se borra el archivo
+					os.remove(fileName)
+					result["error"]="Error: la estacion debe ser de tipo HOBO-MX2300"
+					current_task.update_state(state='FAILURE',meta=result)
+					return result
+
+
+				result["percent"]=10
+				current_task.update_state(state='PROGRESS',meta=result)
+
+				continue;
+
+			# se ignora la linea 2, porque es vacia
+			if(i==2):
+				continue;
+
+			# la tercera linea contiene los headers
+			if(i==3):
+				headers = getColumns(line);
+				if len(headers)!=14:
+					# se borra el archivo
+					os.remove(fileName)
+					msg = "Error: el archivo debe tener ocho columnas, "
+					ms = msg + "se tienen "+str(len(headers)) + " columnas"
+					result["error"]=msg
+					current_task.update_state(state='FAILURE',meta=result)
+					return result
+
+				# se obtiene la informacion del timezone
+				# el string debe ser parseado para obtener el ofset 
+				# en horas
+				header_date = headers[0]
+				index = header_date.find("GMT")
+				time_zone_str = header_date[index:].strip(' \t\n\r')
+				offset_str = time_zone_str[4:7]
+				offset = int(offset_str)
+				# se crea el string del timezone
+				if(offset<0):
+					local_tz_str = "Etc/GMT-" + str(abs(offset));
+				elif(offset >0):
+					local_tz_str = "Etc/GMT+" + str(abs(offset));
+				else:
+					local_tz_str="UTC";
+
+				# se actualiza el progreso
+				result["percent"]=20
+				current_task.update_state(state='PROGRESS',meta=result)
+
+				continue;
+
+			# si la linea es mayor que la 3
+			# se obtienen las mediciones
+			measures = getColumns(line)
+			if(len(measures)!=14):
+				# se borra el archivo
+				os.remove(fileName)
+				result["error"]="Error: falta una columna en la linea "+str(i)
+				current_task.update_state(state='FAILURE',meta=result)
+				return result
+			# se recupera la fecha hora de la primera columna
+			datetime_str = measures[0]
+			try:
+				# se crea un objeto datetime desde el string y el formato
+				dt = datetime.strptime(datetime_str,datetime_format);
+				# se transforma la fecha del time zone local a UTC
+				dt = transformToUTC(dt,local_tz_str);
+			except Exception as e:
+				# se borra el archivo
+				os.remove(fileName)
+				result["error"]="Error: la fecha "+datetime_str+" no es correcta"+str(e)
+				current_task.update_state(state='FAILURE',meta=result)
+				return result
+
+			# en la columna 1 esta temperatura y en la 6, la humedad relativa
+			#variables_index = [1,6]
+			measures_dict = {}
+			measures_len = len(measures)
+			#for i,j in enumerate(variables_index):
+			for j in (1,measures_len):
+				measure = measures[j]
+				measure = measure.strip(' \t\n\r')
+				variable = variables[j] 
+
+				if measure == "":
+					continue;
+
+				idVariable = variable.id;
+				datatype = variable.datatype;
+				measure = parseMeasure(measure,datatype);
+				# se agrega al diccionario de variables y
+				# mediciones
+				measures_dict[idVariable]=measure;
+			
+			# se guardan las mediciones
+			saveMeasurements(station,None,measures_dict, dt);
+
+			# se actualiza el porcentaje de progreso de 30 a 90
+			percent_cont = percent_cont%10
+			percent_cont += 1
+			# cada 50 lineas actualiza el progreso
+			if(percent_cont==0):
+				percent = 30 + (float(i)/num_lines)*75
+				result["error"]=None
+				result["percent"]=percent
+				current_task.update_state(state='PROGRESS',meta=result)
+	except Exception as e:
+		# se borra el archivo
+		print(str(e))
+		os.remove(fileName)
+		result["error"]="Error: el encoding "+encoding + " del archivo no es correcto"
+		current_task.update_state(state='FAILURE',meta=result)
+		return result
+
+	# se completa la tarea
+	result["percent"]=100		
+	return result
+
 
